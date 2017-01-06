20fccc255f33cbbe485482d01588fcbdf2ff2d89
Shrikanth Malarouth <mvnshrikanth@gmail.com>
2017-01-05 21:52:41 -0500

    First Commit

---
+++ app/src/main/java/com/android/sunny/booklisting/MainActivity.java
@@ -0,0 +1,18 @@
+package com.android.sunny.booklisting;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.GridView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        GridView gridView = (GridView) findViewById(R.id.gridView);
+        gridView.setAdapter(new ImageAdapter(this));
+
+    }
+}
