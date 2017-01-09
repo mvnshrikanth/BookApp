928d230bc50521b713f57ba0f16663981882aa78
Shrikanth Malarouth <mvnshrikanth@gmail.com>
2017-01-08 22:22:34 -0500

    remove the grid view

--- app/src/main/java/com/android/sunny/booklisting/MainActivity.java
+++ app/src/main/java/com/android/sunny/booklisting/MainActivity.java
@@ -2,7 +2,6 @@ package com.android.sunny.booklisting;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.GridView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,9 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        GridView gridView = (GridView) findViewById(R.id.gridView);
-        gridView.setAdapter(new ImageAdapter(this));
 
     }
 }
